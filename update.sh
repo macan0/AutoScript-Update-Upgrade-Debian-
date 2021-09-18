36859830815066937ace6df67e411db2dc24db17
macan0 <90868232+macan0@users.noreply.github.com>
2021-09-18 23:16:54 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,3 +4,7 @@ sudo apt update
 sudo apt upgrade
 sudo apt -y autoremove
 sudo apt clean
+
+echo "+-++-++-++-++-++-++-+ +-++-+   +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
+|P||o||w||e||r||e||d| |b||y|   https://github.com/macan0    Need a custom Traccar design: https://www.fiverr.com/users/macano84
++-++-++-++-++-++-++-+ +-++-+   +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+"
